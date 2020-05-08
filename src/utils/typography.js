580bc4aee8a16d75d23e1f3aacf96df3924242c4
Samuel Robertson <sprobertson94@gmail.com>
2020-05-07 20:48:37 -0600

    Need to work out background scroll issue, then done

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -29,16 +29,16 @@ Wordpress2016.overrideThemeStyles = () => {
       boxShadow: `none`,
     },
     "body": {
-      // backgroundImage: `url(../../content/assets/blur-breathtaking-clouds-1903702.jpg)`
-      height: `100vh`,
-      background: `#e0e0e0`,
-      display: `flex`,
-      flexFlow: `column`,
-      alignItems: `center`,
-      justifyContent: `center`
-      // backgroundImage: `url(${image})`,
+      // height: `100vh`,
+      background: `rgb(119,158,203)`,
+      background: `linear-gradient(135deg, rgba(119,158,203,1) 0%, rgba(224,224,224,1) 20%, rgba(224,224,224,1) 52%, rgba(224,224,224,1) 80%, rgba(119,158,203,1) 100%)`,
+      // background: `linear-gradient(135deg, rgba(120,120,120,1) 0%, rgba(224,224,224,1) 20%, rgba(224,224,224,1) 52%, rgba(224,224,224,1) 80%, rgba(120,120,120,1) 100%)`,
+      // display: `flex`,
+      // backgroundPosition: `fixed`,
       // backgroundRepeat: `no-repeat`,
-      // backgroundAttachment: `fixed`,
+      // flexFlow: `column`,
+      // alignItems: `center`,
+      // justifyContent: `center`
     }
   }
 }
